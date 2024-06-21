37011711d02b2acdd298c180368cdf5eab419553
BrunoPolaski <polaskibruno03@gmail.com>
2024-06-21 18:04:48 -0300

    feat: added running symbol

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/BrunoPolaski/go-receiver/controller/routes"
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +12,20 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
+	fmt.Println("Starting application...")
+
 	routes.InitRoute(&r.RouterGroup)
+	go func() {
+		chars := []rune{'|', '\\', '-', '/'}
+		for {
+			for _, char := range chars {
+				fmt.Printf("\r%s", string(char))
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}()
+
+	fmt.Println("\nRunning!")
 
 	if err := r.Run("localhost:8088"); err != nil {
 		panic(err)
