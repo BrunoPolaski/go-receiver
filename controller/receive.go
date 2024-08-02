90bd090c2747321ac51b066059234e26656e4da2
BrunoPolaski <polaskibruno03@gmail.com>
2024-08-01 21:02:45 -0300

    feat: added other routes, fixed headers

--- controller/receive.go
+++ controller/receive.go
@@ -12,14 +12,15 @@ func Receive(c *gin.Context) {
 	fmt.Printf("Request host: %v\n", c.Request.Host)
 	fmt.Printf("Request URL: %v\n", c.Request.URL)
 	fmt.Printf("Request remote address: %v\n", c.Request.RemoteAddr)
-	fmt.Printf("Request content length: %v\n", c.Request.ContentLength)
-	fmt.Printf("Request content type: %v\n", c.Request.Header.Get("Content-Type"))
-	fmt.Printf("Request user agent: %v\n", c.Request.UserAgent())
 	fmt.Printf("Request protocol: %v\n", c.Request.Proto)
-	fmt.Printf("Request authentication: %v\n", c.Request.Header.Get("Authorization"))
-	val, _ := c.GetRawData()
-	fmt.Printf("Request body: %v\n\n", string(val))
-	fmt.Printf("---- End of request ----\n\n")
+	fmt.Println("Request headers:")
+	for k, v := range c.Request.Header {
+		fmt.Printf("  %v: %v\n", k, v)
+	}
+	if val, _ := c.GetRawData(); len(val) > 0 {
+		fmt.Printf("Request body: %v", string(val))
+	}
+	fmt.Printf("\n---- End of request ----\n\n")
 	c.JSON(200, gin.H{
 		"message": "received",
 	})
--- controller/routes/initRoute.go
+++ controller/routes/initRoute.go
@@ -7,4 +7,10 @@ import (
 
 func InitRoute(r *gin.RouterGroup) {
 	r.POST("/receive", controller.Receive)
+	r.GET("/receive", controller.Receive)
+	r.PUT("/receive", controller.Receive)
+	r.DELETE("/receive", controller.Receive)
+	r.PATCH("/receive", controller.Receive)
+	r.OPTIONS("/receive", controller.Receive)
+	r.HEAD("/receive", controller.Receive)
 }
